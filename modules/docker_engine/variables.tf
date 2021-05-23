fd2ef025e87e3f7a328026f4e147eaebc4c4ff7b
cybershard <ashwatdd01@gmail.com>
2021-05-23 00:02:53 -0400

    Docker variable descriptions

--- modules/docker_engine/variables.tf
+++ modules/docker_engine/variables.tf
@@ -1,11 +1,14 @@
-variable "image" { 
+variable "image" {
+  description = "Docker container image"
   type = string
 }
 
 variable "name" {
+  description = "Docker container name"
   type = string
 }
 
 variable "env" {
+  description = "Docker container environment"
   type = list(string)
 }
