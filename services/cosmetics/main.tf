7663e70a0621036592785e58171906e157c7a567
cybershard <ashwatdd01@gmail.com>
2021-06-11 13:15:12 -0400

    use resource instead of module

--- services/cosmetics/main.tf
+++ services/cosmetics/main.tf
@@ -2,17 +2,28 @@ data "digitalocean_ssh_key" "terraform" {
   name = var.ssh_key_name
 }
 
-module "mongodb" {
-  source = "../../modules/digitalocean/droplet"
+resource "digitalocean_droplet" "mongodb" {
+  name   = "cosmetics-mongodb"
+  region = "nyc1"
 
-  name = "cosmetics-mongodb"
-  region_slug  = "nyc1"
-  image = "mongodb-18-04"
-  size_slug = "s-1vcpu-1gb"
+  // Common configuration
+  image  = "mongodb-18-04"
+  size   = "s-1vcpu-1gb"
+  private_networking = true
+  tags = ["cosmetics", "terraform"]
+  
+  // SSH
+  ssh_keys = [ data.digitalocean_ssh_key.terraform.id ]
+  connection {
+    host        = self.ipv4_address
+    user        = "root"
+    type        = "ssh"
+    private_key = var.priv_key
+    timeout     = "2m"
+  }
 
-  tags         = ["cosmetics", "terraform"]
-
-  // SSH Keys
-  ssh_key_ids = [ data.digitalocean_ssh_key.terraform.id ]
-  priv_key    = var.priv_key
+  // Provisioners
+  provisioner "remote-exec" {
+    inline = [ "sudo ufw allow 27017" ]
+  }
 }
