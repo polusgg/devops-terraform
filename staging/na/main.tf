e1030703f9e12720af3f5055d90ce60ba6afce75
cybershard <ashwatdd01@gmail.com>
2021-08-20 13:38:27 -0400

    Update server-nodepolus

--- staging/na/main.tf
+++ staging/na/main.tf
@@ -45,7 +45,7 @@ module "na_west_droplets" {
   }
 
   node_docker = {
-    image    = "registry.digitalocean.com/polusgg/server-nodepolus:v3.0.1-56"
+    image    = "registry.digitalocean.com/polusgg/server-nodepolus:v3.0.1-59"
     env      = [
       "NP_REDIS_HOST=rediss://${module.redis_db.host}",
       "NP_REDIS_PORT=${module.redis_db.port}",
